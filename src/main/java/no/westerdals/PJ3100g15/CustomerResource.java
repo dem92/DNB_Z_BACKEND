0e08ec7e9617a3a87d0691f4ec38139ba00f4069
Daniel Davidsen <danieldavidsen@gmail.com>
2016-10-27 16:57:52 +0200

    Return 404 if customer is not found

--- src/main/java/no/westerdals/PJ3100g15/CustomerResource.java
+++ src/main/java/no/westerdals/PJ3100g15/CustomerResource.java
@@ -1,5 +1,7 @@
 package no.westerdals.PJ3100g15;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +22,16 @@ public class CustomerResource {
 
     @RequestMapping("{id}")
     @ResponseBody
-    public Customer getCustomerById(
+    public ResponseEntity getCustomerById(
             @PathVariable("id") final Long id
     ) {
         for (Customer customer : database.getAllCustomers()) {
             if (customer.getFoedselsnummer() == id) {
-                return customer;
+                return ResponseEntity.ok(customer);
             }
         }
 
-        throw new NullPointerException();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("No user found with id '" + id + "'");
     }
 }
