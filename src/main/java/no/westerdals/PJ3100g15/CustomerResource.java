b78b039ce9fd3796b58bc3924a49bff63eb8833e
Daniel Davidsen <danieldavidsen@gmail.com>
2016-10-27 17:13:09 +0200

    Clean stuff up

--- src/main/java/no/westerdals/PJ3100g15/CustomerResource.java
+++ src/main/java/no/westerdals/PJ3100g15/CustomerResource.java
@@ -1,14 +1,11 @@
 package no.westerdals.PJ3100g15;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-import java.util.function.Supplier;
 
 @Controller
 @RequestMapping("/customers")
