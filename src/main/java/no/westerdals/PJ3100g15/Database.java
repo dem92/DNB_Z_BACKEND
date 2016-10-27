443742515d4997b39fa2eabb027b16c41b474232
Daniel Davidsen <danieldavidsen@gmail.com>
2016-10-27 14:02:48 +0200

    Opprettet forbindelse med database

--- src/main/java/no/westerdals/PJ3100g15/Database.java
+++ src/main/java/no/westerdals/PJ3100g15/Database.java
@@ -1,9 +1,24 @@
 package no.westerdals.PJ3100g15;
 
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
 /**
  * Created by labbetuss on 10/27/16.
  */
 public class Database {
+    // We create a DB connection for further use. TODO - Move the username/password out of this file
+    Sql2o con;
 
 
+    public Database() {
+        con = new Sql2o("jbdc:mysql://tek.westerdals.no:3306/davdan15_pj15", "davdan15_root", "Evaerbest!");
+    }
+
+
+    public List<Customer> getAllCustomers()
+    {
+        return null;
+    }
 }
