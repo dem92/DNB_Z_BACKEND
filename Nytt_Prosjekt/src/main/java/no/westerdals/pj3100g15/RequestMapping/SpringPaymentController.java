9981aefa1fd771fa13b07b61a5b35b439741c5cf
Bragalund <bakhen15@student.westerdals.no>
2016-11-24 11:51:10 +0100

    har slette satan-klassen og flyttet springpaymentcontroller over til requestmapping-package

---
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/RequestMapping/SpringPaymentController.java
@@ -0,0 +1,11 @@
+package no.westerdals.pj3100g15.RequestMapping;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/payments")
+public class SpringPaymentController {
+    
+}
