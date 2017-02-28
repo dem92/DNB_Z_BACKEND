881b275a6a9a681cd812db65d853188cf0af6d2b
Bragalund <bakhen15@student.westerdals.no>
2017-02-28 13:28:37 +0100

    Lagde transferPayment-klassen og controlAccounts-klassen til å håndtere transaksjoner med bigint

--- Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/Transactions/TransactionLogic.java
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/Transactions/TransactionLogic.java
@@ -1,7 +1,27 @@
 package no.westerdals.pj3100g15.Transactions;
 
+import no.westerdals.pj3100g15.ORM.Account;
+
+import java.math.BigInteger;
+
 /**
  * Created by Henrik on 28.02.2017.
  */
 public class TransactionLogic {
+
+    public void transferPayment(Account sending, Account receiving, BigInteger amount) {
+        //både kroner og øre-feltet må oppdateres for begge kontoene.
+        // Benytter subtractAmount og addAmount-metodene i Account-klassen
+        if (controlAccounts(sending, amount)) {
+            sending.setKroner(sending.getKroner().subtract(amount));
+            receiving.setKroner(receiving.getKroner().add(amount));
+        }
+    }
+
+    public boolean controlAccounts(Account sending, BigInteger amount) {
+        if (amount.compareTo(sending.getKroner()) >= 0) {
+            return true;
+        }
+        return false;
+    }
 }
