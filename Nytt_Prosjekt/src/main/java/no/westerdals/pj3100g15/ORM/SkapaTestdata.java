7b69c0f94950fb2e06efa25898556c3c75f84260
craehn <svencraehn@gmail.com>
2017-03-07 13:24:48 +0100

    Har skapat en klass 'SkapaTestdata' i projektet för att skapa testdata till Sparemal-tabellen i databasen. Har kommenterat ut denne pga den har en mainmetod, men den kan användas senare för att fylla i annan testdata om det skulle behövas, då behövs bara lite ändringar i koden.

---
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/ORM/SkapaTestdata.java
@@ -0,0 +1,27 @@
+package no.westerdals.pj3100g15.ORM;
+
+/**
+ * Created by Sven Craehn on 07.03.2017.
+ */
+public class SkapaTestdata {
+/*
+    public static void main(String[] args) {
+
+        String insertString = "INSERT INTO Sparemal (Kontonummer, Navn, KundeID, Mal_kroner, Mal_oere) VALUES ";
+        for (int i = 1; i < 201; i++) {
+            //String accountNumber = DBService.randomNumber();
+            String accountNumber = DBService.addAccount(i, "sparekonto");
+
+            insertString += "(" + accountNumber +
+                    ", 'Ny hest" + "', " +
+                    i + ", " + "25000, " + "45)";
+            if (i == 200)
+                insertString += ";";
+            else
+                insertString += ", ";
+        }
+
+        System.out.println(insertString);
+    }
+    */
+}
