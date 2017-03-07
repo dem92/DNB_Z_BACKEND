902868fde6d9d98b30ba89ad294b0f47f1f2194e
Bragalund <bakhen15@student.westerdals.no>
2017-03-07 10:21:31 +0100

    har lagt til en printwriter i handleException-metoden i ExceptionControllerAdvice-klassen. Den skal skrive til en fil kalt errormessages.txt

--- Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/ExceptionControllerAdvice.java
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/ExceptionControllerAdvice.java
@@ -5,6 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 @ControllerAdvice
 public class ExceptionControllerAdvice {
     @ExceptionHandler
@@ -14,6 +17,13 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler
     public ResponseEntity handleException(final Exception exception) {
+        try{
+        PrintWriter printWriter = new PrintWriter("errormessages.txt", "UTF-8");
+        printWriter.write(exception.getMessage()+" "+exception.toString());
+        printWriter.close();
+        }catch (IOException ioexception){
+            ioexception.printStackTrace();
+        }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage()+" "+exception.toString());
     }
 }
