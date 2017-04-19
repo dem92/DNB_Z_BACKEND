857d42d0ee543e2eedc72a27c9f744ea509b5931
bragalund <bakhen15@student.westerdals.no>
2017-04-19 13:10:32 +0200

    Har kommentert DBConnector med default kommentar.

--- Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/DBService/DBConnector.java
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/DBService/DBConnector.java
@@ -10,34 +10,31 @@ import java.util.Properties;
 
 public class DBConnector {
 
+    /**
+     *
+     * @return
+     */
     public static ConnectionSource makeConnection() {
 
+        //Hardcoded usernames and passwords because of lack of time. This is the worst security EVER, but it works...
         String userName = "daheva15_MainUsr";
         String password = "PJ3100gruppe15!";
         String databaseUrl = "jdbc:mysql://tek.westerdals.no:3306/daheva15_PJ3100_gruppe15";
-/*
-        //Unngår hardkoding i programmet
-        //Leser brukernavn, databaseurl og passord fra properties-fil(db.properties).
-        try {
-            FileReader fileReader = new FileReader("db.properties");
-            Properties properties = new Properties();
-            properties.load(fileReader);
-            userName = properties.getProperty("db.user");
-            password = properties.getProperty("db.password");
-            databaseUrl = properties.getProperty("db.databaseUrl");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-*/
-        //Kaller på privat metode
+
         return connectToDatabase(userName, password, databaseUrl);
     }
 
+    /**
+     *
+     * @param user
+     * @param password
+     * @param databaseUrl
+     * @return
+     */
     public static ConnectionSource connectToDatabase(String user, String password, String databaseUrl) {
         try {
             JdbcPooledConnectionSource connectionSource =
                     new JdbcPooledConnectionSource(databaseUrl, user, password);
-            //ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl, user, password);
             connectionSource.setMaxConnectionAgeMillis(5 * 60 * 1000);
             return connectionSource;
         } catch (Exception e) {
