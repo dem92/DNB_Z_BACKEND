2168f26f02169e60e189e7c1e0dd048b7843a918
bragalund <bakhen15@student.westerdals.no>
2017-04-20 15:54:20 +0200

    Har formattert noen linjer.

--- Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/ServerLogging/WriteLogg.java
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/ServerLogging/WriteLogg.java
@@ -4,10 +4,13 @@ import java.io.*;
 import java.util.Calendar;
 
 public class WriteLogg {
+
+
     /**
      * Creates a textfile, if it does not exists, and writes data about the exception to it.
      * It writes when, what exception it is, the stacktrace and the description of the exception to the file.
      * The purpose is debugging when the server is shutting down when deployed on an external server.
+     *
      * @param exception Exception-object
      */
     public static void writeLogg(Exception exception) {
