54a36dcce536a5fa172d1a2c56484aa634cf7de0
bragalund <bakhen15@student.westerdals.no>
2017-04-20 13:29:31 +0200

    Har lagt til en kommentarer på metodene i DBServiceConnection-klassene.
    Har skrevet en util-metode for å oppdatere customer-objekter i databasen for å fjerne duplisert kode.

--- Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/DBService/DBServiceConnection.java
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/DBService/DBServiceConnection.java
@@ -6,15 +6,21 @@ import no.westerdals.pj3100g15.ServerLogging.WriteLogg;
 import java.io.IOException;
 
 public class DBServiceConnection {
+
     public static ConnectionSource connectionSource;
 
-    //Should be called first thing in every method in this class that requires a working connection to the database.
+    /**
+     * Should be called first thing in every method in this class that requires a working connection to the database.
+     */
     public static void makeConnection() {
         if (connectionSource == null)
             connectionSource = DBConnector.makeConnection();
     }
 
-    public static void closeConnection(){
+    /**
+     * This method is not needed because the connections to the database is timed out.
+     */
+    public static void closeConnection() {
         try {
             connectionSource.close();
         } catch (IOException e) {
--- Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/DBService/DBServiceCustomer.java
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/DBService/DBServiceCustomer.java
@@ -9,9 +9,15 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class DBServiceCustomer {
+
+    /**
+     *
+     * This method creates a connection to the database and retrieves all customers in the database.
+     *
+     * @return 'List<Customer>' a list with customers.
+     */
     public static List<Customer> getAllCustomers() {
         DBServiceConnection.makeConnection();
-
         try {
             Dao<Customer, String> customerDao = DaoManager.createDao(DBServiceConnection.connectionSource, Customer.class);
             List<Customer> customers = customerDao.queryForAll();
@@ -23,6 +29,12 @@ public class DBServiceCustomer {
         return null;
     }
 
+    /**
+     * Creates/reuses a connection to the database and returns the customer with the specified id.
+     *
+     * @param customerId is used to get the customer with the specified id.
+     * @return Customer
+     */
     public static Customer getCustomer(int customerId) {
         DBServiceConnection.makeConnection();
 
@@ -41,6 +53,20 @@ public class DBServiceCustomer {
         return null;
     }
 
+    /**
+     *
+     * Creates a customerobject and sets all the parameters needed to persist it to the database.
+     * It Hardcodes the postalcode, phonenumber, score and address-field,
+     * because it is not as important and can be changed at a later point
+     * by the user.
+     *
+     *
+     * @param firstName
+     * @param surname
+     * @param birthDayNumber
+     * @param email
+     * @return
+     */
     public static boolean addCustomer(String firstName, String surname, String birthDayNumber, String email) {
         DBServiceConnection.makeConnection();
         Customer customer = new Customer();
@@ -64,94 +90,63 @@ public class DBServiceCustomer {
         return false;
     }
 
-    public static boolean updateFirstname(int customerId, String firstname) {
-        DBServiceConnection.makeConnection();
-        Customer customer = getCustomer(customerId);
-        customer.setFirstName(firstname);
+    /**
+     * Utilmethod to persist a customer with changed values.
+     * @param customer the object that is changed
+     * @return boolean
+     */
+    public static boolean updateCustomer(Customer customer){
         try {
             Dao<Customer, Integer> customerDao = DaoManager.createDao(DBServiceConnection.connectionSource, Customer.class);
             customerDao.update(customer);
             return true;
         } catch (SQLException e) {
-            e.printStackTrace();
             WriteLogg.writeLogg(e);
+            e.printStackTrace();
         }
         return false;
+    }
+
+    public static boolean updateFirstname(int customerId, String firstname) {
+        DBServiceConnection.makeConnection();
+        Customer customer = getCustomer(customerId);
+        customer.setFirstName(firstname);
+        return updateCustomer(customer);
     }
 
     public static boolean updateSurname(int customerId, String surname) {
         DBServiceConnection.makeConnection();
         Customer customer = getCustomer(customerId);
         customer.setSurName(surname);
-        try {
-            Dao<Customer, Integer> customerDao = DaoManager.createDao(DBServiceConnection.connectionSource, Customer.class);
-            customerDao.update(customer);
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            WriteLogg.writeLogg(e);
-        }
-        return false;
+        return updateCustomer(customer);
     }
 
     public static boolean updateAddress(int customerId, String address) {
         DBServiceConnection.makeConnection();
         Customer customer = getCustomer(customerId);
         customer.setAddress(address);
-        try {
-            Dao<Customer, Integer> customerDao = DaoManager.createDao(DBServiceConnection.connectionSource, Customer.class);
-            customerDao.update(customer);
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            WriteLogg.writeLogg(e);
-        }
-        return false;
+        return updateCustomer(customer);
     }
 
     public static boolean updatePostalcode(int customerId, int postalcode) {
         DBServiceConnection.makeConnection();
         Customer customer = getCustomer(customerId);
         customer.setPostalCode(postalcode);
-        try {
-            Dao<Customer, Integer> customerDao = DaoManager.createDao(DBServiceConnection.connectionSource, Customer.class);
-            customerDao.update(customer);
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            WriteLogg.writeLogg(e);
-        }
-        return false;
+        return updateCustomer(customer);
     }
 
     public static boolean updateEmail(int customerId, String mail) {
         DBServiceConnection.makeConnection();
         Customer customer = getCustomer(customerId);
         customer.seteMail(mail);
-        try {
-            Dao<Customer, Integer> customerDao = DaoManager.createDao(DBServiceConnection.connectionSource, Customer.class);
-            customerDao.update(customer);
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            WriteLogg.writeLogg(e);
-        }
-        return false;
+        return updateCustomer(customer);
     }
 
     public static boolean updatePhone(int customerId, int phoneNumber) {
         DBServiceConnection.makeConnection();
         Customer customer = getCustomer(customerId);
         customer.setPhoneNumber(phoneNumber);
-        try {
-            Dao<Customer, Integer> customerDao = DaoManager.createDao(DBServiceConnection.connectionSource, Customer.class);
-            customerDao.update(customer);
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            WriteLogg.writeLogg(e);
-        }
-        return false;
+        return updateCustomer(customer);
     }
 
     public static boolean deleteUser(int customerId) {
