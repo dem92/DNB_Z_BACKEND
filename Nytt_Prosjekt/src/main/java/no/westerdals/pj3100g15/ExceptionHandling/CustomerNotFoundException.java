e4a84652d8f65f11fbc9717f236b341cc311dddb
bragalund <bakhen15@student.westerdals.no>
2017-04-20 11:24:45 +0200

    Har kommentert kode i ExceptionHandling-packagen.

--- Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/ExceptionHandling/CustomerNotFoundException.java
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/ExceptionHandling/CustomerNotFoundException.java
@@ -1,6 +1,11 @@
 package no.westerdals.pj3100g15.ExceptionHandling;
 
 public class CustomerNotFoundException extends RuntimeException {
+    /**
+     * Gets the errormessage when a customer is not found in the database.
+     *
+     * @param message is the errormessage.
+     */
     public CustomerNotFoundException(final String message) {
         super(message);
     }
--- Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/ExceptionHandling/ExceptionControllerAdvice.java
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/ExceptionHandling/ExceptionControllerAdvice.java
@@ -8,15 +8,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice {
+
+    /**
+     *  Gets customerNotFoundException and writes it to loggfile.
+     * @param e Exception-object
+     * @return ResponseEntity
+     */
     @ExceptionHandler
     public ResponseEntity handleCustomerNotFoundException(final CustomerNotFoundException e) {
         WriteLogg.writeLogg(e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    /**
+     * Writes down all the internal errors in the spring server to a loggfile.
+     * @param exception the exception-object.
+     * @return ResponseEntity
+     */
     @ExceptionHandler
     public ResponseEntity handleException(final Exception exception) {
-
         WriteLogg.writeLogg(exception);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage() + " " + exception.toString());
     }
