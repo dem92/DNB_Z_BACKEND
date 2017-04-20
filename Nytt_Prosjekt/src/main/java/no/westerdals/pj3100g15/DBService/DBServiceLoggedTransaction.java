c21b279959c4227618bc961e0392435719ed7f25
bragalund <bakhen15@student.westerdals.no>
2017-04-20 15:04:48 +0200

    Har kommentert alle metodene i DBServiceLoggedTransaction.

--- Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/DBService/DBServiceLoggedTransaction.java
+++ Nytt_Prosjekt/src/main/java/no/westerdals/pj3100g15/DBService/DBServiceLoggedTransaction.java
@@ -13,6 +13,7 @@ public class DBServiceLoggedTransaction {
 
     /**
      * If you want to retrieve all the transactions a user have, you can use this method...
+     *
      * @param accountNumber specifies which account.
      * @return a list with LoggedTransaction-objects
      */
@@ -31,21 +32,38 @@ public class DBServiceLoggedTransaction {
         return null;
     }
 
-    public static List<LoggedTransaction> sortTransactions(List<LoggedTransaction> transactions){
-        for(int i = 0; i < transactions.size(); i++){
-            for(int j = 0; j < transactions.size(); j++){
-                if(transactions.get(i).getTimestamp() < transactions.get(j).getTimestamp()){
+    /**
+     * This sorting-method sorts the list based on the timestamp(long).
+     * It places the largest long(the latest timestamp) in the first place of the list
+     * and the latest logged transaction in the last index in the list.
+     * This method compares all the objects in the list and sorts them based on a if-statement("bruteforce"-sorting).
+     * This could be replaced with the quicksort-algorithm for better performance.
+     *
+     * @param transactions is the list with LoggedTransaction-objects that is getting sorted.
+     * @return
+     */
+    public static List<LoggedTransaction> sortTransactions(List<LoggedTransaction> transactions) {
+        for (int i = 0; i < transactions.size(); i++) {
+            for (int j = 0; j < transactions.size(); j++) {
+                if (transactions.get(i).getTimestamp() < transactions.get(j).getTimestamp()) {
                     LoggedTransaction temp = transactions.get(i);
                     transactions.set(i, transactions.get(j));
                     transactions.set(j, temp);
                 }
             }
         }
-
-
         return transactions;
     }
 
+    /**
+     * This method creates a logTransfer-object and persists it to the database.
+     * @param accountNumber is the number to the sending account.
+     * @param accountNumber2 is the number to the receiving account.
+     * @param kroner is the amount of kroner being sent from the one account to the other.
+     * @param oere is the amount of oere being sent from the one account to the other.
+     * @param message is the message/KID associated with the transaction.
+     * @return boolean
+     */
     public static boolean logTransfer(String accountNumber, String accountNumber2, BigInteger kroner, int oere, String message) {
         DBServiceConnection.makeConnection();
         LoggedTransaction loggedTransaction = new LoggedTransaction();
@@ -54,7 +72,6 @@ public class DBServiceLoggedTransaction {
         loggedTransaction.setKroner(kroner);
         loggedTransaction.setOere(oere);
         loggedTransaction.setMessage_kid(message);
-
         loggedTransaction.setTimestamp(System.currentTimeMillis() / 1000L);
 
         if (DBServiceAccount.getAccount(accountNumber).getCustomerNumber() == DBServiceAccount.getAccount(accountNumber2).getCustomerNumber()) {
@@ -67,11 +84,11 @@ public class DBServiceLoggedTransaction {
             loggedTransaction.setTransactionType("card");
         }
 
-        if (accountNumber != null){
+        if (accountNumber != null) {
             loggedTransaction.setSenderID(DBServiceAccount.getAccount(accountNumber).getCustomerNumber());
         }
 
-        if (accountNumber2 != null){
+        if (accountNumber2 != null) {
             loggedTransaction.setReceiverID(DBServiceAccount.getAccount(accountNumber2).getCustomerNumber());
         }
 
